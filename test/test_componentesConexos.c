7d13849de97cf7259751719b143981ec44934d44
Kenai Silva Bazani <silvabazanikenai@gmail.com>
2026-07-09 13:47:21 -0300

    Modulo testes de componentes conexos adicionados, com testes bem basicos

---
+++ test/test_componentesConexos.c
@@ -0,0 +1,42 @@
+#include "../include/componentesConexos.h"
+#include "../include/grafo.h"
+#include "../Unity/unity.h"
+
+static Grafo *g;
+static int chamadas_callback;
+
+static void callback_teste_comp(int comp_idx, double xmin, double ymin, double xmax, double ymax, void *ctx) {
+    chamadas_callback++;
+}
+
+void setUp(void) {
+    g = grafo_create(5);
+    chamadas_callback = 0;
+}
+
+void tearDown(void) {
+    grafo_destroy(g);
+}
+
+void test_componentes_conexos_grafo_vazio_retorna_zero(void) {
+    int num_comp = componentes_conexos_calcular(g, 10.0, callback_teste_comp, NULL);
+    TEST_ASSERT_EQUAL_INT(0, num_comp);
+    TEST_ASSERT_EQUAL_INT(0, chamadas_callback);
+}
+
+void test_componentes_conexos_vertices_isolados(void) {
+    grafo_add_vertice(g, "A", 0.0, 0.0);
+    grafo_add_vertice(g, "B", 10.0, 10.0);
+    
+    // Nenhuma aresta inserida. Devem ser 2 componentes isolados.
+    int num_comp = componentes_conexos_calcular(g, 10.0, callback_teste_comp, NULL);
+    TEST_ASSERT_EQUAL_INT(2, num_comp);
+    TEST_ASSERT_EQUAL_INT(2, chamadas_callback);
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_componentes_conexos_grafo_vazio_retorna_zero);
+    RUN_TEST(test_componentes_conexos_vertices_isolados);
+    return UNITY_END();
+}
